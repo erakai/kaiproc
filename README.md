3e024765190816287f4a894be6b6e903391772cb
Kai Tinkess <kaitinkess@gmail.com>
2024-10-10 13:38:01 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Kai Procedural Generation (KaiProc / KPG)
+
+Exploring various proc gen tools.
+
+## Summary
+
+## Getting Started
+
+**Building**
+
+```bash
+git submodule update --init --recursive
+mkdir build
+cd build
+cmake ..
+make
+```
+
+**Running**
+```bash
+./kaiproc
+```
---
+++ kpg/perlin.cpp
@@ -0,0 +1,14 @@
+#include <math.h>
+
+/*
+This is an implementation of the improved perlin noise algorithm described by
+Ken Perlin in 2002.
+
+His paper can be found: https://mrl.cs.nyu.edu/~perlin/paper445.pdf
+The reference java implementation: https://mrl.cs.nyu.edu/~perlin/noise/
+*/
+
+namespace perlin
+{
+
+}
